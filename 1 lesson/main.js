78e5e6f18a8f30b5dc3352105d9acffc276fbe13
SevaAlieva <pozitiv466@gmail.com>
2022-08-20 19:09:39 +1000

    2 lesson

--- 1 lesson/main.js
+++ 1 lesson/main.js
@@ -3,6 +3,7 @@
 // console.log(123)
 
 // alert("hi")
+// alert("20.08.2022")
 
 
 // var number = 1;
@@ -28,6 +29,7 @@ console.log(-Infinity);
 console.log("hello" / 123);
 console.log(NaN + 5);
 console.log(NaN ** 0);
+// исключение, в результате выдает 1
 
 // BigInt
 let bigi = 1234567n;
@@ -35,15 +37,15 @@ console.log(bigi);
 
 // string(строка - объект, помещенный в кавычки)
 
-let str1 = "hello" + " " + "The Wardo!";
+let str1 = "Welcome" + " " + "on cours" + "," + " "+ "Seva!";
 let str2 = 'hello ${str1}';
 // ``имеют более расширенную функциональность, в отличие от тех двух. В неё можно вставить какое-то выражение при помощи $ и {}
-let str3 = `hello ${str1}`;
+let str3 = `Hello ${str1}`;
 console.log(str1);
 console.log(str2);
 console.log(str3);
 
-// (булевый-логический. Boolean) Logic - True/False
+// (Boolean, булевый-логический) Logic - True/False
 let dotCheck = true;
 console.log(dotCheck);
 let notDotCheck = false;
@@ -55,14 +57,30 @@ let two = 10
 console.log(one < two);
 
 // Null(специальное значение, которое не относится ни к одному из типов данных, описанных выше) Отдельный тип данных.
-//  Не будет ссылкой на несуществующий обЪект(в других ЯП так, нулевой указатель) . Просто показывает, что тут пусто
+//  Не будет ссылкой на несуществующий обЪект(в других ЯП так; нулевой указатель) . Просто показывает, что тут пусто
 
 let nuller = null;
 console.log(nuller);
 
 // undefined(специальное значение. Отдельный тип, говорит о том, что не было присвоено значений)
 let text
-// let text = undefined(можем такое значение просто присвоить, но так лучше не делать) 
-// Обычно используем null, если хотим, чтобы переменная была пустая. Undefined используем для проверки, типо, была ли наша переменная назначена
+//  let text = undefined(можем такое значение просто присвоить, но так лучше не делать) 
+// Обычно используем null, если хотим, чтобы переменная была пустой. Undefined используем для проверки, типо, была ли наша переменная назначена
 console.log(text);
-alert("hi")
+
+// typeof(предназначен для того, чтобы мы узнали текущий тип агрумента. Возвращает тип аргумента)
+// полезен, когда мы хотим обработать различные типы значений по-разному; хотим сделать какое-то тестирование(напр:что за тип д-х нах-ся в перем-й
+// как выглядит форма синтаксического понятия typeof:
+typeof(x)
+// x - значение, которое мы пытаемся вызвать
+typeof x
+
+str1 = false
+
+console.log(typeof (nuller));
+console.log(typeof dotCheck);
+console.log(typeof ("something"));
+console.log(typeof (str1));
+
+document.getElementById("one").innerHTML = "BTS"
+
